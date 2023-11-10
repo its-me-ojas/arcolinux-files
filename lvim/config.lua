b9fcedfac844b6c09823414de796e87286fc7c86
its-me-ojas <crestfallenwizard@gmail.com>
2023-11-11 02:01:21 +0530

    update config.lua

--- lvim/config.lua
+++ lvim/config.lua
@@ -5,32 +5,70 @@
 lvim.transparent_window = false
 lvim.colorscheme = "vscode"
 vim.opt.wrap = true
+
+-- set relative line numbers
+vim.opt.number = true
+vim.opt.relativenumber = true
+
 lvim.plugins = {
-  -- "tiagovla/tokyodark.nvim",
+  "tiagovla/tokyodark.nvim",
   'Mofiqul/vscode.nvim',
   "cpea2506/one_monokai.nvim",
   -- "folke/tokyonight.nvim",
   "catppuccin/nvim",
-  -- "Mofiqul/dracula.nvim",
+  "Mofiqul/dracula.nvim",
+  {
+    'projekt0n/github-nvim-theme',
+    lazy = false,    -- make sure we load this during startup if it is your main colorscheme
+    priority = 1000, -- make sure to load this before all the other start plugins
+    config = function()
+      require('github-theme').setup({
+        -- ...
+      })
+
+      vim.cmd('colorscheme github_dark_high_contrast')
+    end,
+  },
+  {
+    'projekt0n/github-nvim-theme',
+    lazy = false,    -- make sure we load this during startup if it is your main colorscheme
+    priority = 1000, -- make sure to load this before all the other start plugins
+    config = function()
+      require('github-theme').setup({
+        -- ...
+      })
+
+      vim.cmd('colorscheme github_dark')
+    end,
+  },
   {
     "turbio/bracey.vim",
     cmd = { "Bracey", "BraceyStop", "BraceyReload", "BraceyEval" },
     build = "npm install --prefix server",
   },
   {
-    "norcalli/nvim-colorizer.lua",
+    "roobert/tailwindcss-colorizer-cmp.nvim",
+    -- optionally, override the default options:
     config = function()
-      require("colorizer").setup({ "css", "scss", "html", "javascript" }, {
-        RGB = true,      -- #RGB hex codes
-        RRGGBB = true,   -- #RRGGBB hex codes
-        RRGGBBAA = true, -- #RRGGBBAA hex codes
-        rgb_fn = true,   -- CSS rgb() and rgba() functions
-        hsl_fn = true,   -- CSS hsl() and hsla() functions
-        css = true,      -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
-        css_fn = true,   -- Enable all CSS functions: rgb_fn, hsl_fn
+      require("tailwindcss-colorizer-cmp").setup({
+        color_square_width = 2,
       })
-    end,
+    end
   },
+  -- {
+  --   "norcalli/nvim-colorizer.lua",
+  --   config = function()
+  --     require("colorizer").setup({ "css", "scss", "html", "javascript" }, {
+  --       RGB = true,      -- #RGB hex codes
+  --       RRGGBB = true,   -- #RRGGBB hex codes
+  --       RRGGBBAA = true, -- #RRGGBBAA hex codes
+  --       rgb_fn = true,   -- CSS rgb() and rgba() functions
+  --       hsl_fn = true,   -- CSS hsl() and hsla() functions
+  --       css = true,      -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
+  --       css_fn = true,   -- Enable all CSS functions: rgb_fn, hsl_fn
+  --     })
+  --   end,
+  -- },
   {
     "tpope/vim-surround"
   },
@@ -38,6 +76,9 @@ lvim.plugins = {
     'andweeb/presence.nvim'
   },
 
+}
+lvim.builtin.cmp.formatting = {
+  format = require("tailwindcss-colorizer-cmp").formatter
 }
 -- Enable cursor movement in insert mode using CTRL + H/J/K/L
 lvim.keys.insert_mode["<C-h>"] = "<Left>"
